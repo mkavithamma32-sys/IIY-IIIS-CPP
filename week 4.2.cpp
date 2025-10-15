ce02e574d4f04e549beb2f5e01637896f20599fb
mkavithamma32-sys <mkavithamma32@gmail.com>
2025-10-15 18:28:15 +0530

    Add files via upload

---
+++ week 4.2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//Function with default arguments
+void greet(string name = "Using", string greeting = "Welcome")
+{
+	cout <<greeting <<","<<name<<"!"<<endl;
+}
+int main()
+{
+	greet();  //Uses both default arguments
+	greet("Aditya"); //Uses default greeting 
+	greet("Laxman","Hello");  //Uses both custom values
+	return 0;
+}
